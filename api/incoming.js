37a2bc29f69672604d0c160b17a6a68f32880538
AndersonTeodoro-hub <cris7981x@gmail.com>
2025-12-07 12:23:13 +0000

    Update incoming.js

--- api/incoming.js
+++ api/incoming.js
@@ -1,7 +1,14 @@
 export default function handler(req, res) {
   const twiml = `
     <Response>
-      <Say voice="Polly.Camila">Olá, aqui é a Sync. Pode falar, eu estou ouvindo.</Say>
+      <Say voice="Polly.Camila">Olá, aqui é a Sync. Pode falar, estou ouvindo.</Say>
+
+      <Gather input="speech" action="/api/handle-intent" method="POST">
+        <Say voice="Polly.Camila">
+          Você deseja falar com Anderson? Diga conectar Anderson.
+        </Say>
+      </Gather>
+
       <Connect>
         <Stream url="${process.env.NEXT_PUBLIC_BASE_URL}/api/stream" />
       </Connect>
